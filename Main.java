4e27247d670e80d28a7c08e4ba4ff3466df55327
mogan10797 <107021154@gm.asia.edu.tw>
2020-03-18 22:56:06 +0800

    0318

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main{
 public static void main(String[] args) {
     Scanner scn =new Scanner(System.in);
+    ArrayList<Shape> Shapes=new ArrayList<Shape>();
     int r=scn.nextInt();
     System.out.println(area(r));
     float w=scn.nextFloat();
@@ -12,20 +14,28 @@ public static void main(String[] args) {
     float f2=scn.nextFloat();
     System.out.println(area(f1,f2));
     boolean flag=true;
-    while(true){
+    while(flag){
         System.out.println("1.round--2.rect--3.tri--4.quit");
         int cho =scn.nextInt();
         switch(cho){
             case 1:
               System.out.println("round");
+            //    Shapes.add(new Shape(scn.nextInt()));
               System.out.println(Math.PI*r*r);
+              System.out.println("shape"+Shapes.size()+"object");
               break;
               case 2:
-              System.out.println(w*h);
               
+              System.out.println("rect");
+
+              System.out.println(w*h); 
+              System.out.println("shape"+Shapes.size()+"object");             
               break;
               case 3:
+              System.out.println("tri");
+             
               System.out.println(f1*f2/2.0f);
+              System.out.println("shape"+Shapes.size()+"object");
               break;
               case 4:
              flag=false;
--- Shape.java
+++ Shape.java
@@ -2,20 +2,34 @@ import java.util.Scanner;
 
 public class Shape{
 
-private String tri,rect,round;
-
-public Shape(String tri1,String rect1){
-tri=tri1;
-rect=rect1;
+private int tri,rect,round;
+public static int shape1(){return shapetotal;}
+private static int shapetotal=0;
+boolean flag =true;
+public Shape(int round){
+round=round1;
+Scanner scn =new Scanner(System.in);
+int b =scn.nextInt();
+switch(b){
+    case 1:
+    Shape.shapetotal ++;
+    this.flag=true;
+    break;
 }
-public Shape(String tri1,String rect1,String round1){
+}
+public Shape(int tri1,int rect1,int round1){
     tri=tri1;
 rect=rect1;
 round=round1;
 Scanner scn =new Scanner(System.in);
-while(true){
+
 System.out.println("shape");
 int a=scn.nextInt();
+switch(a){
+    case 1:
+    Shape.shapetotal ++;
+    this.flag=true;
+    break;
 }
 }
 }
